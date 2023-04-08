27a5b8888f601dd27e59de577f17d3d6071e5978
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-04-07 23:28:49 -0400

    Created object null test

--- java/practice.java
+++ java/practice.java
@@ -1,5 +1,11 @@
 public class practice {
   public static void main(String args[]) {
+    
     System.out.println("Hacking one program at a time!");
+
+    Object obj = null;
+    String text = obj.toString();
+    System.out.println(text);
+
   }
 }
