c119cbe1fbcc3519693924db3f8cc6ff6717de8f
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-01-09 21:32:11 -0500

    Fixed tabs.

--- python/widget.py
+++ python/widget.py
@@ -2,26 +2,23 @@
 
 """
    Simple widget class
-   
    Author: Daniel Ribeirinha-Braga
 """
 
+
 class Widget:
 
-  thing = None
+    def __init__(self, name):
+        self.name = name
 
-  def __init__(self, name):
-    self.name = name
-
-  def say_name(self):
-    print('Widget is ' + self.name)
+    def say_name(self):
+        print('Widget is ' + self.name)
 
 
 def main():
-  widget = Widget('DBrags')
-  widget.say_name()
+    widget = Widget('DBrags')
+    widget.say_name()
 
 
-if __name__ == "__main__": 
-  main()
-
+if __name__ == "__main__":
+    main()
